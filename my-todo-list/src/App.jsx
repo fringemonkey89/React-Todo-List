8aff5c9215413fb756eb97195c5c6bbb07da28fd
fringemonkey89 <markomajeticjr@gmail.com>
2023-11-07 18:30:05 -0500

    fixed bugs in fewtodoform

--- my-todo-list/src/App.jsx
+++ my-todo-list/src/App.jsx
@@ -1,25 +1,19 @@
-import { useState } from "react"
+import React, { useState } from "react"
+import { NewTodoForm } from "./NewTodoForm"
 
 
 export default function App() {
-  const [newItem, setNewItem] = useState("")
-  const [todos, setTodos] = useState([]) 
-function handleSubmit(e) {
-  e.preventDefault()
-  
-  // setTodos([...todos, {id: crypto.randomUUID(), title:newItem, completed: false}
-  // ])
 
+const [todos, setTodos] = useState([]) 
+
+function addTodo(title) {
   setTodos(currentTodos => {
-    return [
+    return[
       ...currentTodos,
-      {id: crypto.randomUUID(), title: newItem, completed:false},
+      {id: crypto.randomUUID(), title, completed: false },
     ]
   })
-
-  setNewItem("")
 }
-
 function toggleTodo(id, completed){
   setTodos(currentTodos => {
     return currentTodos.map(todo => {
@@ -40,15 +34,10 @@ function deleteTodo(id) {
 console.log(todos)
   return (
   <>
-  <form onSubmit = {handleSubmit} className="new-item-form">
-    <div className = "form-row">
-        <label htmlFor="item">New Item</label>
-        <input value = {newItem} onChange= {e => setNewItem(e.target.value)} type = "text" id="item"/>
-    </div>
-  <button className="btn">Add Item</button>
-  </form>
+  <NewTodoForm onSubmit={addTodo}/>
   <h1 className="header">Todo List</h1>
   <ul className="list">
+    {todos.length === 0 && "no to - dos "}
     {todos.map(todo =>{
       return <li key={todo.id}>
       <label>
