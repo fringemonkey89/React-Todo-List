2d65d5eea4c816e2134328aadce597cb6c164f34
fringemonkey89 <markomajeticjr@gmail.com>
2023-11-07 11:16:56 -0500

    added more code to app.js

--- my-todo-list/src/App.js
+++ my-todo-list/src/App.js
@@ -1,13 +1,33 @@
+import { useState } from "react"
 
 
 export default function App() {
-  return <form className="new-item-form">
+  const [newItem, setNewItem] = useState("")
+
+function handleSubmit(e) {
+  e.preventDefault()
+}
+  return (
+  <>
+  <form onSubmit = {handleSubmit} className="new-item-form">
     <div className = "form-row">
         <label htmlFor="item">New Item</label>
-        <input type = "text" id="item"/>
+        <input value = {newItem} onchange= {e => setNewItem(e.target.value)} type = "text" id="item"/>
     </div>
   <button className="btn">Add Item</button>
   </form>
+  <h1 className="header">Todo List</h1>
+  <ul className="list">
+    <li>
+      <label>
+        <input type="checkbox"/>
+        Item 1
+      </label>
+      <button className="btn btn-danger">delete</button>
+    </li>
+  </ul>
+  </>
+  )
 }
 
 
