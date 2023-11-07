6a6621dbe1be12ed163cd34e81087537c5508000
fringemonkey89 <markomajeticjr@gmail.com>
2023-11-07 12:15:08 -0500

    added more content to app.js file

--- my-todo-list/src/App.js
+++ my-todo-list/src/App.js
@@ -3,28 +3,43 @@ import { useState } from "react"
 
 export default function App() {
   const [newItem, setNewItem] = useState("")
-
+  const [todos, setTodos] = useState([]) 
 function handleSubmit(e) {
   e.preventDefault()
+  
+  // setTodos([...todos, {id: crypto.randomUUID(), title:newItem, completed: false}
+  // ])
+
+  setTodos(currentTodos => {
+    return [
+      ...currentTodos,
+      {id: crypto.randomUUID(), title: newItem, completed:false},
+    ]
+  })
 }
+
+console.log(todos)
   return (
   <>
   <form onSubmit = {handleSubmit} className="new-item-form">
     <div className = "form-row">
         <label htmlFor="item">New Item</label>
-        <input value = {newItem} onchange= {e => setNewItem(e.target.value)} type = "text" id="item"/>
+        <input value = {newItem} onChange= {e => setNewItem(e.target.value)} type = "text" id="item"/>
     </div>
   <button className="btn">Add Item</button>
   </form>
   <h1 className="header">Todo List</h1>
   <ul className="list">
-    <li>
+    {todos.map(todo =>{
+      return <li>
       <label>
-        <input type="checkbox"/>
-        Item 1
+        <input type="checkbox" checked={todo.completed}/>
+        {todo.title }
       </label>
       <button className="btn btn-danger">delete</button>
     </li>
+    })}
+
   </ul>
   </>
   )
